3169007e462249767d6a8c33aedf970b3513eefd
Salvador Guzman <guzmansalv@gmail.com>
2017-11-13 01:56:47 -0800

    added main stub

---
+++ src/main/scala/com/github/sguzman/leetcode/scraper/Main.scala
@@ -0,0 +1,14 @@
+package com.github.sguzman.leetcode.scraper
+
+import org.jsoup.Jsoup
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val url = "https://leetcode.com/problemset/all/"
+    println(get(url))
+  }
+
+  def get(url: String) = {
+
+  }
+}
