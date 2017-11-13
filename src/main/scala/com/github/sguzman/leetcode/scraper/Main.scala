36a0baf86790cfee43487af16716a7f2075974fe
Salvador Guzman <guzmansalv@gmail.com>
2017-11-13 05:20:39 -0800

    decoupled method calls

--- src/main/scala/com/github/sguzman/leetcode/scraper/Main.scala
+++ src/main/scala/com/github/sguzman/leetcode/scraper/Main.scala
@@ -1,24 +1,31 @@
 package com.github.sguzman.leetcode.scraper
 
-import com.github.sguzman.leetcode.scraper.jsontype.Leetcode
+import com.github.sguzman.leetcode.scraper.jsontype.{Difficulty, Leetcode, Stat}
 import com.google.gson.GsonBuilder
-import com.mashape.unirest.http.Unirest
+import com.mashape.unirest.http.{JsonNode, Unirest}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val url = "https://leetcode.com/api/problems/all/"
-    get(url).foreach(t => println(format(t)))
+
+    val resp = Main.get(url)
+    val l33t = Main.toLeetcode(resp)
+    val stats = Main.toStats(l33t)
+    val sorted = Main.sortById(stats)
+    val tups = Main.idAndTitle(sorted)
+
+    tups.foreach(t => println(format(t)))
   }
 
-  def get(url: String): Array[(Int, String)] = {
-    val resp = Unirest.get(url).asJson.getBody
-    val gson = new GsonBuilder().create
-    val json = gson.fromJson(resp.toString, classOf[Leetcode])
-    val problems = json.stat_status_pairs.map(_.stat)
-    val sorted = problems.sortBy(_.question_id)
-    val zip = sorted.map(_.question_id).zip(sorted.map(_.question__title))
-    zip
-  }
+  private def get(url: String): JsonNode = Unirest.get(url).asJson.getBody
 
-  def format(t: (Int, String)): String = s"Leetcode ${t._1}. ${t._2}"
+  private def toLeetcode(json: JsonNode) = new GsonBuilder().create.fromJson(json.toString, classOf[Leetcode])
+
+  private def toStats(l33t: Leetcode) = l33t.stat_status_pairs.map(_.stat)
+
+  private def sortById(stats: Array[Stat]) = stats.sortBy(_.question_id)
+
+  private def idAndTitle(xs: Array[Stat]) = xs.map(_.question_id).zip(xs.map(_.question__title))
+
+  private def format(t: (Int, String)): String = s"Leetcode ${t._1}. ${t._2}"
 }
